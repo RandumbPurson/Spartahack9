be3e3c907097c5cdf3070c8fd58c09f557316ffa
EC2 Default User <ec2-user@ip-172-31-34-201.us-east-2.compute.internal>
2024-01-27 22:12:03 +0000

    Allow chuckl

--- chuckl/chuckl/settings.py
+++ chuckl/chuckl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%4k*(vfn2owfcl!2r!(!d_%3d^ye+6oj-za$ml(v4jtmu^(wmc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".chuckl.club"]
 
 
 # Application definition
