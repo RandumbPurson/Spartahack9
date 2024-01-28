0ed705fe88067181f5d2770ae04e7792dafbf6a1
Emma Fountain <randumbemma@gmail.com>
2024-01-27 21:12:21 -0500

    Add backend utils

--- chuckl-flask/chuckl.py
+++ chuckl-flask/chuckl.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from NextImg import ImageNavigator
+from User import User
 
 app = Flask(__name__)
+
+user = User()
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/memes", methods=["GET", "PUT"])
+def next_meme():
+    print(request)
+    if request.method == "GET":
+        return nav.find_next_unseen_image()
+    if request.method == "PUT":
+        user.updatePrefs(request.get_json())
+        
